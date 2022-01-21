1f96e3728bb850f764ce92ce27383684dde87532
Vanessa Stewart <vstewart1213@gmail.com>
2022-01-20 20:51:01 -0800

    Add variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-const {src, dest, wathc, series, watch} = require('gulp');
+const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
   return src('index.scss')
     .pipe(sass())
     .pipe(dest('css'))
-};
+}
 
 function watchTask() {
   watch(['index.scss'], buildStyles)
 }
 
-exports.default = series(buildStyles, watchTask);
+exports.default = series(buildStyles, watchTask)
