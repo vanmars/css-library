a9e5b3f2f81082024225c74f4f9f91eafb8d0162
Vanessa Stewart <vstewart1213@gmail.com>
2022-01-20 21:07:21 -0800

    Improve file structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
+  return src('jamila/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(['jamila/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
